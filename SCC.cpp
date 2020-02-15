8704ea0b378245927c7ebb280187eca1387d4e8a
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-15 23:17:58 +0600

    Add files via upload

--- SCC.cpp
+++ SCC.cpp
@@ -5,79 +5,98 @@
 using namespace std;
 int G[100][100];
 int GT[100][100];
-int color[100], d[100], f[100], prev[100],c[100];
+int color[100], d[100], f[100], previous[100],counter[100];
+int color2[100], d2[100], f2[100], previous2[100];
 int component[100];
 int top=0;
 int time1=0;
-int count1=0;
-    int n, e, u, v;
+int count2=0;
+int time2=0;
+int n, e, u, v;
 
 /*void print_path(int s, int v) {
     if(s==v) {
        // printf("%d->",s);
        return 1;
     }
-    else if(prev[v]==-1) {
+    else if(previous[v]==-1) {
             return 0;
        // printf("No Path");
     }
     else {
-        print_path(s,prev[v]);
+        print_path(s,previous[v]);
 
      //   printf("%d->",v);
     }
 }*/
 
-void DFS_visit2(int u, int n,int count1) {
-    time1++;
-    d[u]=time1,
-    color[u]=GRAY;
-    for(int v=0;v<n;v++) {
-        if(G[u][v]==1) {
-            if(color[v]==WHITE)
+void DFS_visit2(int u, int n)
+{
+    time2++;
+    d2[u]=time2,
+          color2[u]=GRAY;
+
+
+    for(int v=0; v<n; v++)
+    {
+        if(GT[u][v]==1)
+        {
+            if(color2[v]==WHITE)
             {
-                prev[v]=u;
-               //c[u]=pic;
-                DFS_visit2(v,n,count1);
+                previous2[v]=u;
+                //counter[u]=pic;
+                DFS_visit2(v,n);
             }
         }
-    }
-    time1++;
-    f[u]=time1;
+        time2++;
+        f2[u]=time2;
 
-    color[u]=BLACK;
-    c[u]=count1;
+        color2[u]=BLACK;
+        counter[u]=count2;
+
+    }
+
+
 }
 
 void DFS2(int n)
 {
 
-  for(int i=0;i<n;i++) {
-        color[i]=WHITE;
-        prev[i]=-1;
+    for(int i=0; i<n; i++)
+    {
+        color2[i]=WHITE;
+        previous2[i]=-1;
+        d2[i]=0;
+        f2[i]=0;
     }
 
-while(top!=0){
+    while(top!=0)
+    {
 
-    u=component[top];
-    top--;
-}        if(color[u]==WHITE)
+        u=component[top];
+        top--;
+
+        if(color2[u]==WHITE)
         {
-            count1++;
-        DFS_visit2(u,n,count1);
+            DFS_visit2(u,n);
+        count2++;
 
         }
+    }
 
 }
-void DFS_visit(int u, int n) {
+void DFS_visit(int u, int n)
+{
     time1++;
     d[u]=time1,
-    color[u]=GRAY;
-    for(int v=0;v<n;v++) {
-        if(G[u][v]==1) {
+         color[u]=GRAY;
+    for(int v=0; v<n; v++)
+    {
+        if(G[u][v]==1)
+        {
             if(color[v]==WHITE)
             {
-                prev[v]=u;
+                previous[v]=u;
                 DFS_visit(v,n);
             }
         }
@@ -92,45 +111,51 @@ void DFS_visit(int u, int n) {
 void DFS(int n)
 {
 
-  for(int i=0;i<n;i++) {
+    for(int i=0; i<n; i++)
+    {
         color[i]=WHITE;
-        prev[i]=-1;
+        previous[i]=-1;
+        d[i]=0;
+        f[i]=0;
     }
 
-    for(int u=0;u<n;u++) {
+    for(int u=0; u<n; u++)
+    {
         if(color[u]==WHITE)
         {
-        DFS_visit(u,n);
+            DFS_visit(u,n);
 
         }
     }
 }
 
 
-int main() {
+int main()
+{
     printf("Enter number of nodes: ");
     scanf("%d",&n);
     printf("Enter number of edges: ");
     scanf("%d",&e);
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0; j<n; j++)
         {
             G[i][j]=0;
+            GT[i][j]=0;
         }
     }
 
-    for(int i=0;i<e;i++)
+    for(int i=0; i<e; i++)
     {
-        printf("Enter edges:");
+        // printf("Enter edges:");
         scanf("%d %d",&u,&v);
         G[u][v]=1;
         GT[v][u]=1;
     }
 
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0; j<n; j++)
         {
             printf("%-4d",G[i][j]);
         }
@@ -139,15 +164,35 @@ int main() {
 
     DFS(n);
 
-DFS2(n);
-for(int i=0;i<=count1;i++){
-    cout<<endl<<"Forest "<<i<<" :";
-    for(int j=0;j<n;j++){
-        if(c[j]==i){
-            cout<<j<<" ";
+    DFS2(n);
+    for(int i=0; i<count2; i++)
+    {
+        cout<<endl<<"Forest "<<i<<" :";
+        for(int j=0; j<n; j++)
+        {
+            if(counter[j]==i)
+            {
+                cout<<j<<" ";
+            }
         }
     }
-}
 
     return 0;
 }
+
+/*
+1 0
+0 2
+2 3
+1 3
+3 5
+5 3
+4 5
+2 4
+4 6
+6 4
+6 7
+7 7
+5 7
+2 1
+*/
