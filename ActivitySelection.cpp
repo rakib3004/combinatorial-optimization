60fc0cad32fdabda43b0045fb9687941fbdb1a93
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-02-16 21:38:59 +0600

    Add files via upload

--- ActivitySelection.cpp
+++ ActivitySelection.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printMaxActivities(int s_array[],int f_array[],int n){
+void printMaxActivities(int StartingTimeArray[],int FinishingTImeArray[],int n){
 int i,j;
 
 i=0;
@@ -9,7 +9,7 @@ i=0;
 cout<<i;
 
 for(j=1;j<n;j++){
-    if(s_array[j]>=f_array[i]){
+    if(StartingTimeArray[j]>=FinishingTImeArray[i]){
         cout<<j<<" ";
         i=j;
     }
@@ -20,9 +20,9 @@ for(j=1;j<n;j++){
 
 int main(){
 
-int s_array[] = {1,3,0,5,8,5};
-int f_array[]={2,4,6,7,9,9};
-int n = sizeof(s_array)/sizeof(s_array[0]);
-printMaxActivities(s_array,f_array,n);
+int StartingTimeArray[] = {1,3,0,5,8,5};
+int FinishingTImeArray[]={2,4,6,7,9,9};
+int n = sizeof(StartingTimeArray)/sizeof(StartingTimeArray[0]);
+printMaxActivities(StartingTimeArray,FinishingTImeArray,n);
 return 0;
 }
