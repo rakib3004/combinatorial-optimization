e05aba4480f45ab2a4aa98be7a3e17e39e0b5b81
rakib3004 <bsse1129@iit.du.ac.bd>
2020-07-23 19:55:03 +0600

    add C project  instrument 2.0

---
+++ HomeWork/cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/NetworkFlow.cpp.o"
+  "untitled"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
