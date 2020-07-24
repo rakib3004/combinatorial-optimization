30d769ef7115a94d56e88c7b1f3e866fdf5dc8cc
rakib3004 <bsse1129@iit.du.ac.bd>
2020-07-24 20:45:05 +0600

    re-arrange Floyd-Warshall-Path.cpp

--- HomeWork/Floyd-Warshall-Path.cpp
+++ HomeWork/Floyd-Warshall-Path.cpp
@@ -1,73 +1,87 @@
-
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-int n,e;
-cout<<"Number of Nodes : ";
-cin>>n;
-cout<<endl;
+int main()
+{
+    int n,e;
+    cout<<"Number of Nodes : ";
+    cin>>n;
+    cout<<endl;
 
-int f[n][n],c[n][n],path[n][n],flag[n][n],weight[n][n];
-int i,j;
+    int f[n][n],c[n][n],path[n][n],flag[n][n],weight[n][n];
+    int i,j;
 
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-weight[i][j]=1000000000;
-path[i][j]=j;
-}
-}
-for(i=0;i<n;i++){
-weight[i][i]=0;
-}
-int x,y,flow,w;
-cout<<"Number of edges :";
-cin>>e;
-cout<<endl;
-cout<<"From-->To-->Weight"<<endl;
-for(i=0;i<e;i++){
-cin>>x>>y>>w;
-weight[x][y]=w;
-}
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            weight[i][j]=1000000000;
+            path[i][j]=j;
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        weight[i][i]=0;
+    }
+    int x,y,flow,w;
+    cout<<"Number of edges :";
+    cin>>e;
+    cout<<endl;
+    cout<<"From-->To-->Weight"<<endl;
+    for(i=0; i<e; i++)
+    {
+        cin>>x>>y>>w;
+        weight[x][y]=w;
+    }
 
-int k;
+    int k;
 
-for(k=0;k<n;k++){
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-if(weight[i][j]>(weight[i][k]+weight[k][j])){
-weight[i][j]= (weight[i][k]+weight[k][j]);
-path[i][j]=path[i][k];
-}
-}
-}
-}
-cout<<"From-->To-->Minimum cost"<<endl;
+    for(k=0; k<n; k++)
+    {
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<n; j++)
+            {
+                if(weight[i][j]>(weight[i][k]+weight[k][j]))
+                {
+                    weight[i][j]= (weight[i][k]+weight[k][j]);
+                    path[i][j]=path[i][k];
+                }
+            }
+        }
+    }
+    cout<<"From-->To-->Minimum cost"<<endl;
 
-for(i=0;i<n;i++){
-for(j=0;j<n;j++){
-if(weight[i][j]&&weight[i][j]!=1000000000){
-cout<<i<<"--->"<<j<<"===>"<<weight[i][j]<<endl;
-}
-}
-}
-int a,b;
-cout<<"Source Vertex : ";
-cin>>a;
-cout<<"Sink Vertex : ";
-cin>>b;
-if(weight[a][b]!=1000000000){
-cout<<"Path is : "<<a;
-while(a!=b){
-a=path[a][b];
-cout<<"-->"<<a;
-}
-cout<<endl;
-}
-else{
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            if(weight[i][j]&&weight[i][j]!=1000000000)
+            {
+                cout<<i<<"--->"<<j<<"===>"<<weight[i][j]<<endl;
+            }
+        }
+    }
+    int a,b;
+    cout<<"Source Vertex : ";
+    cin>>a;
+    cout<<"Sink Vertex : ";
+    cin>>b;
+    if(weight[a][b]!=1000000000)
+    {
+        cout<<"Path is : "<<a;
+        while(a!=b)
+        {
+            a=path[a][b];
+            cout<<"-->"<<a;
+        }
+        cout<<endl;
+    }
+    else
+    {
 
-cout<<"No Path is found"<<endl;
-}
-return 0;
+        cout<<"No Path is found"<<endl;
+    }
+    return 0;
 
 }
 /*
