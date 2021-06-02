40be65158ed303e5618d15cbbf7f0f74134813ef
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-06-02 09:50:03 +0600

    add vertext and node in pRIMS aLGORITHM

--- PrimsAlgorithm.cpp
+++ PrimsAlgorithm.cpp
@@ -1,10 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+int graph[100][100];
 int main()
 {
 
+graph[1][6]=54;
+graph[1][3]=47;
+graph[1][5]=80;
+graph[2][3]=55;
+graph[2][4]=31;
+graph[2][5]=32;
+graph[2][7]=74;
+graph[2][8]=79;
+graph[3][4]=88;
+graph[3][5]=23;
+graph[3][6]=75;
+graph[3][7]=66;
+graph[4][6]=74;
+graph[4][8]=29;
+graph[5][7]=93;
+graph[7][8]=68;
+
+int i=0,j=0;
+
+
+for(i=1;i<=8;i++){
+    for(j=1;j<=7;j++){
+        if(graph[i][j]){
+
+        }
+    }
+}
 
 
     return 0;
