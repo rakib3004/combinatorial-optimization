fd8bfdab5936ee872e24bcd2fc68a969cc0d5333
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2021-06-05 20:38:26 +0600

    add array in dijekstra

--- DijkstraAlgorithm.cpp
+++ DijkstraAlgorithm.cpp
@@ -1,12 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int graph[100][100];
+int distance[100];
+int pathHistory[100];
 
 int main()
 {
+    graph[1][8]=14;
+    graph[1][6]=45;
+    graph[1][3]=11;
+    graph[2][4]=17;
+    graph[2][5]=25;
+    graph[3][6]=20;
+    graph[3][5]=33;
+    graph[2][8]=40;
+    graph[1][4]=16;
+    graph[5][8]=29;
+    graph[7][3]=24;
+    graph[7][5]=14;
+    graph[7][4]=36;
 
 
 
     return 0;
 
 }
+/*
+
+Graph 1(Array Methods):
+
+
+
+    graph[1][6]=54;
+    graph[1][3]=47;
+    graph[1][5]=80;
+    graph[2][3]=55;
+    graph[2][4]=31;
+    graph[2][5]=32;
+    graph[2][7]=74;
+    graph[2][8]=79;
+    graph[3][4]=88;
+    graph[3][5]=23;
+    graph[3][6]=75;
+    graph[3][7]=66;
+    graph[4][6]=74;
+    graph[4][8]=29;
+    graph[5][7]=93;
+    graph[7][8]=68;
+
+
+    Example 2(Console Methods):
+
+        1 8 14
+        1 6 45
+        1 3 11
+        2 4 17
+        2 5 25
+        3 6 20
+        3 5 33
+        2 8 40
+        1 4 16
+        5 8 29
+        7 3 24
+        7 5 14
+        7 4 36
+*/
--- KruskalAlgorithm.cpp
+++ KruskalAlgorithm.cpp
@@ -1,12 +1,108 @@
+
 #include<bits/stdc++.h>
 using namespace std;
+
+int graph[100][100];
+int cost[100];
+int previous[100];
+int visit[100];
+
+int totalComponent=0;
+
+struct graphValue
+{
+
+    int x;
+    int y;
+    int cost;
+    int point;
+
+} graphComponent[100];
+
+
+void sorting()
+{
+    int i=0,j=0;
+    graphValue temporaryGraph;
+    for(i=1; i<=totalComponent; i++)
+    {
+
+        for(j=i+1; j<=totalComponent; j++)
+        {
+
+            if(graphComponent[j].cost<graphComponent[i].cost)
+            {
+                temporaryGraph=graphComponent[j];
+                graphComponent[j]=graphComponent[i];
+                graphComponent[i]=temporaryGraph;
+            }
+        }
+    }
+}
 
 
 int main()
 {
 
+    graph[1][6]=54;
+    graph[1][3]=47;
+    graph[1][5]=80;
+    graph[2][3]=55;
+    graph[2][4]=31;
+    graph[2][5]=32;
+    graph[2][7]=74;
+    graph[2][8]=79;
+    graph[3][4]=88;
+    graph[3][5]=23;
+    graph[3][6]=75;
+    graph[3][7]=66;
+    graph[4][6]=74;
+    graph[4][8]=29;
+    graph[5][7]=93;
+    graph[7][8]=68;
 
+    int n=8;
+
+
+    int i,j;
+    int k=1;
+    for(i=1; i<=8; i++)
+    {
+
+        for(j=1; j<=8; j++)
+        {
+
+            if(graph[i][j])
+            {
+                graphComponent[k].x=i;
+                graphComponent[k].y=j;
+                graphComponent[k].cost=graph[i][j];
+                graphComponent[k].point=i;
+                k++;
+            }
+        }
+    }
+
+
+    totalComponent=k-1;
+    sorting();
+int MinimumSpanningTree=0;
+
+    for(i=1;i<=totalComponent; i++)
+    {
+        int  a= graphComponent[i].x;
+        int  b=graphComponent[i].y;
+        if(graphComponent[a].point!=graphComponent[b].point)
+        {
+              graphComponent[b].point  =graphComponent[a].point;
+              MinimumSpanningTree+=graphComponent[i].cost;
+        }
+    }
+
+
+
+    cout<<"Total Cost : "<<MinimumSpanningTree<<endl;
 
     return 0;
-
 }
+
