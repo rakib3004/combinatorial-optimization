b67ba25815553fd09ef26b049de15e1ea280f75d
rakib3004 <bsse1129@iit.du.ac.bd>
2022-02-14 10:25:56 +0600

    create kd tree

---
+++ KD-Tree.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void test()
+{
+
+
+    return;
+
+}
+int main()
+{
+
+
+    int t;
+
+    cin>>t;
+
+    while(t--)
+    {
+
+        test();
+
+    }
+
+
+    return 0;
+
+}
